69a51f5f89be26014b39417cfae46e6623c6273b
simoncdesmet <simoncdesmet@gmail.com>
2019-11-04 11:09:24 +0100

    initial commit

---
+++ war/src/test/java/com/simon/eurder/war/eurderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simon.eurder.war;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class eurderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
